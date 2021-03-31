1bbbe45a52f0cf003b479ac09b909e124987dfc8
Samuelsonvs <sametleader611@hotmail.com>
2021-03-31 12:20:03 +0300

    mobile navbar fix

--- src/components/layouts/navbarMobileToggler.js
+++ src/components/layouts/navbarMobileToggler.js
@@ -54,27 +54,27 @@ export default function NavbarMobileToggler() {
             <nav id="mobile-nav" onClick={menuFunc} ref={container} className={'top-0 right-0 h-screen absolute z-30 bg-gray-700 text-white font-semibold sm:text-3xl md:hidden  transition duration-500 ease-in-out '
              + (menu === false ? 'transform translate-x-full scale-0 opacity-0' : 'transform translate-x-0 w-1/2')}> 
                 <ul>
-                <li className="mt-80 mb-3 p-4 text-center hover:bg-gray-400">
+                <li className="mt-80 mb-3 text-center ">
+                    <Link className="block w-full p-4 hover:bg-gray-400" to="/">
                     <FiHome className="inline mr-2" />
-                    <Link to="/">
                     Anasayfa
                     </Link>
                 </li>
-                <li className="mt-5 mb-3 p-4 text-center hover:bg-gray-400">
+                <li className="mt-5 mb-3 text-center">
+                    <Link className="block w-full p-4 hover:bg-gray-400" to="/ilanlar">
                     <FiColumns className="inline mr-2" />
-                    <Link to="/ilanlar">
                     Türler
                     </Link>
                 </li>
-                <li className="mt-5 mb-3 p-4 text-center hover:bg-gray-400">
+                <li className="mt-5 mb-3 text-center">
+                    <Link className="block w-full p-4 hover:bg-gray-400" >
                     <FiMapPin className="inline mr-2" />
-                    <Link >
                     İletişim
                     </Link>
                 </li>
-                <li className="mt-5 mb-3 p-2 text-center hover:bg-gray-400">
+                <li className="mt-5 mb-3 text-center">
+                    <Link className="block w-full p-4 hover:bg-gray-400" >
                     <FiSmile className="inline mr-2" />
-                    <Link >
                     Hakkımızda
                     </Link>
                 </li>
